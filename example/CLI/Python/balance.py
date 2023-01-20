30d41a8edafbaa228d47ce63fa35fd3d593b54a7
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2023-01-20 19:38:45 +0700

    🚚 Move to `example\CLI\Python`

--- example/CLI/Python/balance.py
+++ example/CLI/Python/balance.py
@@ -1,27 +1,32 @@
-import requests
 import os
 import json
+
+try:
+    import requests
+except ImportError:
+    os.system("pip install requests")
+    print("\033[0;31m!! Please re-run the command again !!\033[0m")
 
 # !!
 print("\033[0;31m!! Please don't leak your access token to anyone !!\033[0m")
 # !!
 
-sAccessToken = input("Please enter your access token: ")
-os.system("cls")
+sAccessToken = input("Please enter your access token: ") # Asking for access token...
+os.system("cls") # Clear the console
 
-url = "https://app-api.salad.io/api/v1/profile/balance"
+url = "https://app-api.salad.io/api/v1/profile/balance" # Set API URL
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
-}
+} # Set User Agent
 
 cookies = {
     # sIdRefreshToken is used to refresh the authentication token, it not needed for API requests unless you're trying to refresh the token.
     "sIdRefreshToken": "a",
     "sAccessToken": sAccessToken
-}
+} # Set cookies
 
-response = requests.get(url=url, headers=headers, cookies=cookies)
+response = requests.get(url=url, headers=headers, cookies=cookies) # Get response
 
 if response.status_code != 200:
     print('It doesn\'t look like that was successful!')
